8c361ac8c3e975498a1174682f05f35193b1fdfa
Vijeesh <vichus301@gmail.com>
2025-02-21 23:29:37 +0530

    latest-commit-night

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "build",
+  },
+  base: "/billerTest/",
   plugins: [
     tailwindcss(),
     react()
   ],
-  base: "https://vijeesh301.github.io/billerTest/"
 })

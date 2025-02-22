83f89047bf6df9e85e7f332b5a8dd4eae679d938
Vijeesh <vichus301@gmail.com>
2025-02-22 07:52:35 -0800

    latest-commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename={https://github.com/Vijeesh301/billerTestprocess.env.PUBLIC_URL}>
+    <BrowserRouter basename="/Vijeesh301/">
       <App />
     </BrowserRouter>
   </StrictMode>

8bb9ec0463a68cea98e579b77047c305ffa0f6fc
Vijeesh <vichus301@gmail.com>
2025-02-22 09:44:00 +0530

    morning

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
+  // <StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  // </StrictMode>
 );
